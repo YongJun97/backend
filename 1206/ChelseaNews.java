8101c1e4a97b0d3e0fdd155615c0e2b2ff5c2d8f
YongJun97 <yyj1098@gmail.com>
2024-12-12 17:32:06 +0900

    a1212

--- 1206/ChelseaNews.java
+++
@@ -1,8 +0,0 @@
-package mini.chelsea;
-
-public class ChelseaNews {
-    public void showNews() {
-        System.out.println("Chelsea FC 뉴스");
-
-    }
-}
--- 1206/TicketPurchase.java
+++
@@ -1,8 +0,0 @@
-package mini.chelsea;
-
-public class TicketPurchase {
-    public void purchaseTicket() {
-        System.out.println("티켓 구매");
-    }
-}
-
